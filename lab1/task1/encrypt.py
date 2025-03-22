7c0cd66725b0f88399cb1cb27d9610a330a4d8a0
Deemeed <dmitrijcencov82500@gmail.com>
2025-03-22 12:12:34 +0400

    vigenere decryption

--- lab1/task1/encrypt.py
+++ lab1/task1/encrypt.py
@@ -12,4 +12,14 @@ def encrypt_vigenere(data: str, key: str, alphabet: str) -> str:
     return result
 
 def decrypt_vigenere(data, key, alphabet):
-    pass
+    result = ""
+
+    data = data.upper()
+    for i in range(len(data)):
+        if data[i] in alphabet:
+            value = alphabet[(alphabet.index(data[i]) + len(alphabet) - alphabet.index(key[i % len(key)])) % len(alphabet)]
+            result += value
+        else:
+            result += data[i]
+
+    return result
