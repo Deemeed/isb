4c2cbb08ef880ff034db7cfc7c820cb7fed81808
Deemeed <dmitrijcencov82500@gmail.com>
2025-03-29 19:35:30 +0400

    main file changes

--- lab1/main.py
+++ lab1/main.py
@@ -3,23 +3,32 @@ from task1.encrypt import encrypt_vigenere
 from task2.decrypt import get_frequency, get_key, decrypt
 
 
+def task1() -> None:
+    task1 = readjson("settings.json").get("task1")
+
+    data = readfile(task1["plaintext"])
+    key = readjson(task1["key"]).get("key")
+    alphabet = readjson(task1["key"]).get("alphabet")
+
+    ciphertext = encrypt_vigenere(data, key, alphabet)
+    writefile(task1["ciphertext"], ciphertext)
+
+
+def task2() -> None:
+    task2 = readjson("settings.json").get("task2")
+
+    data = readfile(task2["ciphertext"])
+    writejson(task2["frequency"], get_frequency(data))
+    key = get_key(get_frequency(data), readjson(task2["russian_frequency"]))
+    writejson(task2["key"], key)
+    key = readjson(task2["key"])
+    writefile(task2["plaintext"], decrypt(data, key))
+
+
 def main():
     try:
-        task1 = readjson("settings.json").get("task1")
-        task2 = readjson("settings.json").get("task2")
-
-        data = readfile(task1["plaintext"])
-        key = readjson(task1["key"]).get("key")
-        alphabet = readjson(task1["key"]).get("alphabet")
-        ciphertext = encrypt_vigenere(data, key, alphabet)
-        writefile(task1["ciphertext"], ciphertext)
-
-        #data = readfile(task2["ciphertext"])
-        #writejson(task2["frequency"], get_frequency(data))
-        #key = get_key(get_frequency(data), readjson(task2["russian_frequency"]))
-        #writejson(task2["key"], key)
-        #key = readjson(task2["key"])
-        #writefile(task2["plaintext"], decrypt(data, key))
+        task1()
+        task2()
     except Exception as exc:
         print(f'Something went wrong: {exc}')
 
