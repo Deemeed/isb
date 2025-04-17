73d724392ae6159cb2392243c95c52cb4f71d740
Deemeed <dmitrijcencov82500@gmail.com>
2025-04-17 13:58:02 +0400

    identical consecutive bit test

--- lab2/task2/NIST_tests.py
+++ lab2/task2/NIST_tests.py
@@ -15,7 +15,20 @@ def frequency_bit_test(sequence: str) -> float:
 
 
 def identical_consecutive_bit_test(sequence: str) -> float:
-    pass
+    n = len(sequence)
+    z = sequence.count("1") / n
+
+    if abs(z - 0.5) < (2 / sqrt(n)):
+        v = 0
+        for i in range(n - 1):
+            if sequence[i] != sequence[i+1]:
+                v += 1
+
+        p_value = erfc(abs(v - 2 * n * z * (1 - z)) / (2 * sqrt(2 * n) * z * (1 - z)))
+        return p_value
+    else:
+        p_value = 0
+        return p_value
 
 
 def longest_sequence_of_ones_test(sequence: str) -> float:
