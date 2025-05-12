1bca29d01ce5040d76e2ef18412676e748af7871
Deemeed <dmitrijcencov82500@gmail.com>
2025-05-12 11:34:00 +0400

    simmetric/asimmetric methods

---
+++ lab3/asimmetric.py
@@ -0,0 +1,23 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+
+
+def generate_asimmetric_keys(key_len: str) -> tuple[str, str]:
+    keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    private_key = keys
+    public_key = keys.public_key()
+
+    return private_key, public_key
+
+
+def encrypt_simmetric_key(public_key: str, simmetric_key: str, key_len: str):
+    key = bytes(simmetric_key, 'UTF-8')
+    c_key = public_key.encrypt(key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+
+    return c_key
+
+
+def decrypt_simmetric_key(private_key: str, c_key: str):
+    dc_key = private_key.decrypt(c_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+
+    return dc_key
---
+++ lab3/simmetric.py
@@ -0,0 +1,42 @@
+import os
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+def generate_simmetric_key() -> str:
+    key = os.urandom(32)
+
+    return key
+
+
+def padding_text(text: str) -> str:
+    padder = padding.ANSIX923(32).padder()
+    text = bytes(text, 'UTF-8')
+    padded_text = padder.update(text) + padder.finalize()
+
+    return padded_text
+
+
+def encrypt_text(text: str, key: str) -> str:
+    padded_text = padding_text(text)
+
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.Camellia(key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    c_text = encryptor.update(padded_text) + encryptor.finalize()
+
+    return c_text
+
+
+def decrypt_text(c_text: str, cipher=None) -> str:
+    decryptor = cipher.decryptor()
+    dc_text = decryptor.update(c_text) + decryptor.finalize()
+
+    return unpadding_text(dc_text)
+
+
+def unpadding_text(dc_text: str) -> str:
+    unpadder = padding.ANSIX923(32).unpadder()
+    unpadded_dc_text = unpadder.update(dc_text) + unpadder.finalize()
+
+    return unpadded_dc_text
